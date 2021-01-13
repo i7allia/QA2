faa41a8d543e3237e433b392a065278fbe6af38d
i7allia <76435852+i7allia@users.noreply.github.com>
2021-01-13 16:51:05 +0300

    Создал класс Employee
    Внутри класса делаю проверку по полям:
    a.	Стаж работы - поле не должно быть отрицательным числом.
    b.	Уровень квалификации - после должно быть числом от 1 до 5

---
+++ EmployeeApplication/EmployeeApplication/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeApplication
+{
+    class Employee
+    {
+        int Work_experience;
+        int Skill_level;
+        string Last_name;
+        string Name;
+
+        public Employee(int Work_experience, int Skill_level, string Last_name, string Name)
+        {
+            if ((Work_experience >= 0) & (Skill_level > 0) & (Skill_level < 6))
+            {
+                this.Work_experience = Work_experience;
+                this.Skill_level = Skill_level;
+                this.Last_name = Last_name;
+                this.Name = Name;
+            }
+            else
+            {
+                Console.WriteLine("Check the Field 1. Work experience - cannot be a negative number. Field 2. Skill level - can be chilo only from 1-5.!");
+                this.Work_experience = -1;
+                this.Skill_level = -1;
+
+            }
+        }
+            public int getWork_experience()
+            {
+                return this.Work_experience;
+            }
+
+    }
+}
