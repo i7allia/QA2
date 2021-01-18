27c3d6cc9ad1608bd04af19e197bf7dd5194d8b9
i7allia <76435852+i7allia@users.noreply.github.com>
2021-01-18 13:31:55 +0300

    while для проверки на отрицательные числа
    Дописал цикл while при не выполнение условия ввода данных в поля Skill_level, Work_experience

--- EmployeeApplication/EmployeeApplication/Employee.cs
+++ EmployeeApplication/EmployeeApplication/Employee.cs
@@ -14,18 +14,26 @@ namespace EmployeeApplication
 
         public Employee(int Work_experience, int Skill_level, string Last_name, string Name, int BonusPercent)
         {
-            if ((Work_experience >= 0) & (Skill_level > 0) & (Skill_level < 6))
+            while (true)
+            {
+
+                if ((Work_experience >= 0) & (Skill_level > 0) & (Skill_level < 6))
             {
                 this.Work_experience = Work_experience;
                 this.Skill_level = Skill_level;
                 this.Last_name = Last_name;
                 this.Name = Name;
-                            }
+                    break;
+                }
             else
             {
                 Console.WriteLine("Сотрудник не создан по одной из причин: Проверьте поле 1. Опыт работы - не может быть отрицательным числом. Поле 2. Уровень квалификации - может быть чило только от 1-5. Все поля обязательны для заполнения");
-                       
 
+                Console.WriteLine("Повторить? y/n");
+                if (Console.ReadKey(true).Key != ConsoleKey.Y)
+                       
+                    Environment.Exit(0);
+                }
             }
 
            
